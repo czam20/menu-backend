e1396288f63e618992289a2f12823831d64fa99a
Branyer <branyervergarac@gmail.com>
2024-06-09 13:07:46 -0400

    models

---
+++ models/Restaurant.mjs
@@ -0,0 +1,93 @@
+import mongoose, { Schema } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+const PlateSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+  },
+  categories: {
+    type: [String],
+    enum: ["entrada", "principal", "postre", "bebida"],
+    required: true,
+  },
+  isRecommendation: {
+    type: Boolean,
+    default: false,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const OrderSchema = new mongoose.Schema({
+  platos: [PlateSchema],
+  client: {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    dni: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+  },
+  totalAmmount: {
+    type: Number,
+    required: true,
+  },
+  table: {
+    type: Number,
+    required: true,
+  },
+});
+
+const RestaurantSchema = new mongoose.Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  rif: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  logo: {
+    type: String,
+  },
+  plates: {
+    type: [PlateSchema],
+    default: () => ([]),
+  },
+  orders: {
+    type: [OrderSchema],
+    default: () => ([]),
+  },
+});
+
+RestaurantSchema.plugin(uniqueValidator);
+
+const RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+export default RestaurantModel;
---
+++ models/User.mjs
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+const UserSchema = new mongoose.Schema({
+  rol: {
+    type: String,
+    enum: {
+      values: ["owner", "waiter"],
+      message: "{VALUE} no es válido",
+    },
+    required: true,
+  },
+  fullname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: "Email inválido",
+    },
+  },
+  dni: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+UserSchema.plugin(uniqueValidator);
+
+const UserModel = mongoose.model("User", UserSchema);
+export default UserModel;
