96367efea1198886812434d2c18e10681fb25488
Branyer <branyervergarac@gmail.com>
2024-06-09 21:27:22 -0400

    cors

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import "./loadEnvironment.mjs";
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/db.mjs";
 import { UserRouter } from "./routes/user/index.mjs";
 import { RestaurantRouter } from "./routes/restaurant/index.mjs";
@@ -11,6 +12,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use("/api/users", UserRouter);
 app.use("/api/restaurant", RestaurantRouter);
 app.use("/api/auth", AuthRouter);
